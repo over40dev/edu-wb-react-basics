467ae8bd39d489ce25f9078ae5c0dc13ec1b97a8
over40dev <over40dev.com>
2020-04-14 23:01:21 -0230

    RFB2-Lesson-07 complete

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header></Header>
+          <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order></Order>
         <Inventory></Inventory>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
-class Header extends React.Component {
-  render() {
-    return (
+const Header = props => (
       <header className="top">
         <h1>
           Catch
@@ -13,12 +11,9 @@ class Header extends React.Component {
           Day
         </h1>
         <h3 className="tagline">
-          <span>Fresh Seafood</span>
+          <span>{props.tagline}</span>
         </h3>
       </header>
     );
-  }
-
-}
 
 export default Header;
