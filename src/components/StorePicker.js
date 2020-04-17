d08ab0075bbd84a543835c83763afdf516501fcf
over40dev <over40dev@gmail.com>
2020-04-16 23:52:38 -0230

    FB2-Lesson-11-events, refs, binding - complete

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,12 +1,42 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends Component {
+
+  constructor() {
+    super();
+    this.goToStore = this.goToStore.bind(this);
+  }
+  state = {
+    storeCount: 0,
+  };
+
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    // 2. get the text from the input
+    console.log(this.myInput);
+
+    // 3.
+    console.log(`going to store`, event);
+  }
+
   render() {
     return (
-      <form className="store-selector">
-        <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name"/>
-        <button type="submit">Visit Store <span aria-label="right-arrow">➡️</span></button>
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Select Store</h2>
+        <input
+          type="text"
+          required
+          placeholder="Store Name"
+          ref = {this.myInput}
+          defaultValue={getFunName()}
+        />
+        <button type="submit">
+          Visit Store <span aria-label="right-arrow">➡️</span>
+        </button>
       </form>
     );
   }
